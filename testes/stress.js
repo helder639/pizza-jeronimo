aba2e1622e2054197dfff12e22a30f635d8cdb63
Helder <neto0309@hotmail.com>
2025-10-10 16:41:08 -0300

    Teste de Stress

---
+++ testes/stress.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '60s', target: 200 }, // ramp up to 200 users over 1 minutes
+    { duration: '4m', target: 200 }, // stay at 200 users for 4 minutes
+    {duration: '30s', target: 0 },
+    ],
+};
+
+export default function () {
+    const res = http.get('https://thankful-bay-039b7071e.2.azurestaticapps.net/menu.html');
+    check(res, { 'status was 200': (r) => r.status === 200 });
+    sleep(1);
+}
